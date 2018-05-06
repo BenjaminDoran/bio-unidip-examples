72aa23d18cb01e058a0b276c824c3475a8f5d07d
BenjaminDoran <benadoran@gmail.com>
2018-05-06 00:47:38 -0400

    improve example

--- runmeme.sh
+++ runmeme.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# example command
-# date && ./runmeme.sh meme /home/biodev/MA0852_2/MA0852.2SEQS1K.fasta -dna -oc /home/biodev/MA0852_2/raw -maxsize 6000000 -mod oops -nmotifs 3 -minw 6 -maxw 30 && date 
+# example command for raw sequences:
+#
+# date && \
+# ./runmeme.sh meme /home/biodev/MA0852_2/MA0852.2SEQS1K.fasta -dna -oc /home/biodev/MA0852_2/raw -maxsize 6000000 -mod oops -nmotifs 3 -minw 6 -maxw 30 \
+# && date
+#
 
 docker run --rm -v $(pwd):/home/biodev/ ddiez/meme "$@"
 
